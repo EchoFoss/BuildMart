7d8a8ad5a705f42b3d761169bd6ddfe8c3ea8868
fernando <fernando.balieiro@outlook.com>
2024-04-06 15:36:45 -0300

    criadas algumas funcoes novas

--- src/app/get-all-products/get-all-products.component.ts
+++ src/app/get-all-products/get-all-products.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ProductsService} from "../useCases/productUseCases/products-service.service";
+import {ProdutoModel} from "../Models/Produto/produto-model";
+import {FormsModule} from "@angular/forms";
+import {PostProductComponent} from "../post-product/post-product.component";
 
 @Component({
   selector: 'app-get-all-products',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    PostProductComponent
+  ],
   templateUrl: './get-all-products.component.html',
   styleUrl: './get-all-products.component.scss'
 })
 export class GetAllProductsComponent {
+  public useCase: ProductsService;
+
+  public allProducts: readonly ProdutoModel[]
+  constructor(useCase: ProductsService) {
+    this.useCase = useCase
+    this.allProducts = this.useCase.readAll()
+  }
+
+  deletar(id: string) {
+    this.useCase.delete(id);
+  }
+
+  atualizar(input: ProdutoModel) {
+    this.useCase.update(input);
+  }
 
 }
--- src/app/post-product/post-product.component.ts
+++ src/app/post-product/post-product.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {AbstractServiceInterface} from "../useCases/abstract-service.interface";
+import {ProdutoModel} from "../Models/Produto/produto-model";
+import {ProductsService} from "../useCases/productUseCases/products-service.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-post-product',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './post-product.component.html',
   styleUrl: './post-product.component.scss'
 })
 export class PostProductComponent {
+  public useCase: ProductsService
+  @Input() nameProduto: string = "";
+  @Input() descricao: string = "";
+  @Input() preco: number = 0;
+  constructor(useCase: ProductsService) {
+    this.useCase = useCase;
+
+  }
+
+  submit(): void {
+    try {
+      const newProduct = ProdutoModel.new(this.nameProduto, this.descricao, this.preco);
+      this.useCase.add(newProduct)
+      console.log(newProduct)
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
 }
